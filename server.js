66a67571aa79b493ac64b18c60c30a3f9e1abf26
holliemaethomas <holliemaethomas85@gmail.com>
2019-12-09 11:40:28 -0800

    server.js fix

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 'use strict';
 
+// const express = require('express');
+// const app = express();
+
+// app.use(express.static('./public'));
+
+// app.get('/webpage', function(request, response){
+//   response.sendFile(__dirname + 'index.html');
+// });
+
+
+// const PORT = process.env.PORT || 3000;
+
 const express = require('express');
-const app = express();
-
-app.use(express.static('./public'));
-
-app.get('/webpage', function(request, response){
-  response.sendFile(__dirname + '/public/index.html');
-});
-
-
+const path = require('path');
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, function() {
-  console.log('great job nicholas, you started a server on port 3000');
-});
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index.html'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
