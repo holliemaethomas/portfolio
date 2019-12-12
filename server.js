79fee645405e2541016e58213bfdbcbc206e0e66
holliemaethomas <holliemaethomas85@gmail.com>
2019-12-12 08:08:02 -0800

    save work

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-// 'use strict';
-// const express = require('express');
-// // const path = require('path');
-// const PORT = process.env.PORT || 3002;
+'use strict';
+const express = require('express');
+// const path = require('path');
+const PORT = process.env.PORT || 3002;
 
-// const app = express();
+const app = express();
 
-// app.use('/static', express.static('assets'));
-// app.use('/images', express.static('images'));
-// app.use('/asset', express.static('base'));
-// app.use('/', express.static('public'));
+app.use('/static', express.static('assets'));
+app.use('/images', express.static('images'));
+app.use('/assets', express.static('base'));
+app.use('/', express.static('public'));
 
 
-// app.get('/', (req, res) => {
-//   res.send('hello')
-// })
+app.get('/', (req, res) => {
+  res.send('hello')
+})
 
-//   // app.set('views', path.join(__dirname, 'views'))
-// app.set('view engine')
-// // app.get('/', (req, res) => res.serve('index'))
+  // app.set('views', path.join(__dirname, 'views'))
+app.set('view engine')
+// app.get('/', (req, res) => res.serve('index'))
   
 
 
@@ -25,4 +25,4 @@
 
 
 
-// app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
