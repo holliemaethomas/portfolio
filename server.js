a26623db439c059e9fea6ce325f0a8cf3b7a46b5
holliemaethomas <holliemaethomas85@gmail.com>
2019-12-26 13:47:40 -0800

    took out server.js

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-'use strict';
-const express = require('express');
+// 'use strict';
+// const express = require('express');
 // const path = require('path');
-const PORT = process.env.PORT || 3002;
+// const PORT = process.env.PORT || 3002;
 
-const app = express();
+// const app = express();
 
-app.use('/static', express.static('assets'));
-app.use('/images', express.static('images'));
-app.use('/assets', express.static('base'));
-app.use('/', express.static('public'));
+// app.use('/static', express.static('assets'));
+// app.use('/images', express.static('images'));
+// app.use('/assets', express.static('base'));
+// app.use('/', express.static('public'));
 
 
-app.get('/', (req, res) => {
-  res.send('hello')
-})
+// app.get('/', (req, res) => {
+//   res.send('hello')
+// })
 
   // app.set('views', path.join(__dirname, 'views'))
-app.set('view engine')
+// app.set('view engine')
 // app.get('/', (req, res) => res.serve('index'))
   
 
@@ -25,4 +25,4 @@ app.set('view engine')
 
 
 
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
